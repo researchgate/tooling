1e5d79b6334fd9bcabd4a6687a8d59f3ccbbe769
Sergey Bekrin <sergey.bekrin@researchgate.net>
2019-04-04 15:09:45 +0200

    fix(jest-preset): set correct rootDir

--- packages/jest-preset/jest-preset.js
+++ packages/jest-preset/jest-preset.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const defaults = require('spire-plugin-jest/jest-preset');
+
 module.exports = {
-  name: 'researchgate',
+  ...defaults,
   setupFilesAfterEnv: [require.resolve('./setup-after-env')],
   modulePathIgnorePatterns: ['<rootDir>/.cache/'],
 };
