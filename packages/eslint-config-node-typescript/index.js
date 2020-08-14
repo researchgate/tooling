0ce7d65b6d0efff6db06c4c2bd0147f032ce00ea
Daniel Tschinder <daniel.tschinder@researchgate.net>
2020-08-14 17:46:32 +0200

    fix(eslint): Proper setup for node typescript projects

--- packages/eslint-config-node-typescript/index.js
+++ packages/eslint-config-node-typescript/index.js
@@ -2,7 +2,23 @@
 
 module.exports = Object.assign(
   {
-    extends: ['@researchgate/eslint-config-node'],
+    extends: '@researchgate/eslint-config-node',
+    parserOptions: { sourceType: 'module' },
+    rules: {
+      'node/no-unsupported-features': 'off',
+      strict: ['error', 'never'],
+    },
+    overrides: [
+      {
+        files: ['**/.eslintrc.js', '**/*.config.js'],
+        rules: {
+          strict: ['error', 'global'],
+        },
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
   },
   require('@researchgate/eslint-settings-typescript')
 );
