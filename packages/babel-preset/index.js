84fbfe97a0c991eb30911066b3dd509afeb27ffd
Daniel Tschinder <daniel.tschinder@researchgate.net>
2020-09-07 14:52:20 +0200

    fix: Fix invalid broken output type

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -2,8 +2,7 @@
 
 module.exports = function (context, options = {}) {
   const env = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
-  const type =
-    process.env.BABEL_OUTPUT || (env === 'test' ? 'commonjs' : 'esm');
+  const type = process.env.BABEL_OUTPUT || (env === 'test' ? 'cjs' : 'esm');
   if (type !== 'esm' && type !== 'cjs') {
     throw new Error(
       [
