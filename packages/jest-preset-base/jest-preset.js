1c89c49ecb1cbd38bdf2ae99f998903222238593
Daniel Tschinder <daniel.tschinder@researchgate.net>
2020-10-05 13:07:47 +0200

    fix(jest): Configure proper test environment

--- packages/jest-preset-base/jest-preset.js
+++ packages/jest-preset-base/jest-preset.js
@@ -4,6 +4,7 @@ const defaults = require('spire-plugin-jest/jest-preset');
 
 module.exports = {
   ...defaults,
+  testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/.cache/'],
   setupFilesAfterEnv: [require.resolve('./setup-after-env')],
 };
--- packages/jest-preset-react/jest-preset.js
+++ packages/jest-preset-react/jest-preset.js
@@ -4,5 +4,6 @@ const defaults = require('@researchgate/jest-preset-base');
 
 module.exports = {
   ...defaults,
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: [require.resolve('./setup-after-env')],
 };
